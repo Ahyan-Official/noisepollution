f095a4d15fe0341c1f3c79011fdfba10c4018ff5
Azron Ahyan <azronahyan@gmail.com>
2022-06-13 11:30:49 -0700

    Noise Meter It detect noise levels Maps to display Noise levels of different location Third Octave Graph

---
+++ app/src/main/java/com/app/noisepollution/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.app.noisepollution;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+    }
+
+
+
+
+
+}
