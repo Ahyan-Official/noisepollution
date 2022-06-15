9ed6b407daf8266f1903e4812f97fd5d5638bfe6
Azron Ahyan <azronahyan@gmail.com>
2022-06-15 14:52:53 +0500

    Noise Meter It detect noise levels Maps to display Noise levels of different location Third Octave Graph Splash Screen Fixed

--- app/src/main/java/com/app/noisepollution/SplashActivity.java
+++ app/src/main/java/com/app/noisepollution/SplashActivity.java
@@ -22,11 +22,58 @@ public class SplashActivity extends AppCompatActivity {
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        isAudioPermissionGranted();
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent( SplashActivity.this,     MapsActivity.class));
+                finish();
+                overridePendingTransition(0, 0);
+            }
+        }, secondsDelayed * 3500);
 
     }
 
 
+    public  boolean isAudioPermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+                //Log.v(TAG,"Permission is granted2");\
+                return true;
+            } else {
 
+                //Log.v(TAG,"Permission is revoked2");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 3);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            //Log.v(TAG,"Permission is granted2");
+            return true;
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 3: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(         SplashActivity.this,
+                            Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+
+                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
+    }
 
 }
